1425c459e56a3e49db0bc69a94b484d7af392b03
Hao Wu <haowu@HaodeMacBook-Air.local>
2024-06-21 15:21:42 +0800

    clean

---
+++ clean.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+find "Results/plots" -type f -delete
+find "Results/complete" -type f -delete
+find "Results/completesemi" -type f -delete
+find "Results/sound" -type f -delete
+find "Results/systems" -type f -delete
+
+echo "clean computation results"
